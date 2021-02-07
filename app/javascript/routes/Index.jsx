ca75a164c6b519511bca98808d012ebb60eaffbd
NikitaYaskin <nikuuu66@gmail.com>
2021-02-07 22:54:50 +0200

    Fix error

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../components/Home";
 import Recipes from "../components/Resipes";
+import Recipe from "../components/Recipe";
 
 export default (
   <Router>
